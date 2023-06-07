fd76c04e1bc670fcec8c5398787a95bd99bc4051
matthewhand <matthewhandau@gmail.com>
2023-06-07 20:13:57 +1000

    Refactor python_todo_list.py to be importable as a module

--- project/utils/python_todo_list.py
+++ project/utils/python_todo_list.py
@@ -1,24 +1,29 @@
+
 import os
 import re
 import argparse
 
-# Define the command line arguments
-parser = argparse.ArgumentParser()
-parser.add_argument('--root_dir', default='.', help='Root directory to search for TODO comments')
-parser.add_argument('--todo_regex', default=r'^\s*#\s*TODO\s*(.*)$', help='Regular expression to match TODO comments')
-args = parser.parse_args()
+def find_todo_comments(root_dir='.', todo_regex=r'^\s*#\s*TODO\s*(.*)$'):
+    # Loop through all files in the root directory and its subdirectories
+    for dirpath, dirnames, filenames in os.walk(root_dir):
+        for filename in filenames:
+            # Only process Python files
+            if filename.endswith('.py'):
+                # Read the file contents
+                filepath = os.path.join(dirpath, filename)
+                with open(filepath, 'r') as f:
+                    contents = f.read()
+                # Search for TODO comments
+                matches = re.findall(todo_regex, contents, re.MULTILINE)
+                # Print the TODO comments
+                for match in matches:
+                    print(match)
 
-# Loop through all files in the root directory and its subdirectories
-for dirpath, dirnames, filenames in os.walk(args.root_dir):
-    for filename in filenames:
-        # Only process Python files
-        if filename.endswith('.py'):
-            # Read the file contents
-            filepath = os.path.join(dirpath, filename)
-            with open(filepath, 'r') as f:
-                contents = f.read()
-            # Search for TODO comments
-            matches = re.findall(args.todo_regex, contents, re.MULTILINE)
-            # Print the TODO comments
-            for match in matches:
-                print(match)
+if __name__ == '__main__':
+    # Define the command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--root_dir', default='.', help='Root directory to search for TODO comments')
+    parser.add_argument('--todo_regex', default=r'^\s*#\s*TODO\s*(.*)$', help='Regular expression to match TODO comments')
+    args = parser.parse_args()
+
+    find_todo_comments(args.root_dir, args.todo_regex)
